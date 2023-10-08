154e8d2b7fc3ad7f70240b727f43a73b4aabfe04
Zero <tobewhatwewant@outlook.com>
2023-10-08 19:34:25 +0800

    chore: rename cmd

--- cmd/command-runner/main.go
+++ cmd/command-runner/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"github.com/go-zoox/cli"
 	"github.com/go-zoox/command"
-	"github.com/go-zoox/command/cmd/cmd/commands"
+	"github.com/go-zoox/command/cmd/command-runner/commands"
 )
 
 func main() {
 	app := cli.NewMultipleProgram(&cli.MultipleProgramConfig{
-		Name:    "cmd",
+		Name:    "command-runner",
 		Usage:   "Powerful command runner",
 		Version: command.Version,
 	})
