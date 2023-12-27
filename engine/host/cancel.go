7f9f06d6168b96785ff174dc8ba61474710aaed2
Zero <tobewhatwewant@gmail.com>
2023-12-27 10:22:26 +0800

    fix: host engine cancel if process is nil

--- engine/host/cancel.go
+++ engine/host/cancel.go
@@ -2,6 +2,10 @@ package host
 
 // Cancel cancels the command.
 func (h *host) Cancel() error {
+	if h.cmd.Process == nil {
+		return nil
+	}
+
 	if err := h.cmd.Process.Kill(); err != nil {
 		return err
 	}
