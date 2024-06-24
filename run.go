f0fd81a4d4448fd5157b501cc2c07ad3474d4cb9
Zero <tobewhatwewant@gmail.com>
2024-06-24 11:27:30 +0800

    chore: move timeout logic from run to wait

--- run.go
+++ run.go
@@ -1,32 +1,9 @@
 package command
 
-import (
-	"time"
-
-	"github.com/go-zoox/core-utils/fmt"
-)
-
 // Run runs the command.
 func (c *command) Run() error {
 	if err := c.Start(); err != nil {
 		return err
-	}
-
-	if c.cfg.Timeout != 0 {
-		done := make(chan error)
-		go func() {
-			done <- c.Wait()
-		}()
-
-		select {
-		case <-c.cfg.Context.Done():
-			return c.cfg.Context.Err()
-		case <-time.After(c.cfg.Timeout):
-			c.Cancel()
-			return fmt.Errorf("timeout to run command (command: %s, timeout: %s)", c.cfg.Command, c.cfg.Timeout)
-		case err := <-done:
-			return err
-		}
 	}
 
 	return c.Wait()
--- wait.go
+++ wait.go
@@ -1,6 +1,28 @@
 package command
 
+import (
+	"fmt"
+	"time"
+)
+
 // Wait waits for the command to exit.
 func (c *command) Wait() error {
+	if c.cfg.Timeout != 0 {
+		done := make(chan error)
+		go func() {
+			done <- c.engine.Wait()
+		}()
+
+		select {
+		case <-c.cfg.Context.Done():
+			return c.cfg.Context.Err()
+		case <-time.After(c.cfg.Timeout):
+			c.Cancel()
+			return fmt.Errorf("timeout to run command (command: %s, timeout: %s)", c.cfg.Command, c.cfg.Timeout)
+		case err := <-done:
+			return err
+		}
+	}
+
 	return c.engine.Wait()
 }
